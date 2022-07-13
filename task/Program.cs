5c85189844be5527eac5761b13f71f5928936a53
ekaterina <zaitsevae1609@gmail.com>
2022-07-13 14:57:41 +0700

    create method

---
+++ task/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 
+
+string[] GetWord(string[] array, int n)
+{
+    string[] outputArray = new string[array.Length];
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= n)
+        {
+            outputArray[index] = array[i];
+            index++;
+        }
+    }
+    return outputArray;
+}
+
+System.Console.WriteLine("Введит число N");
+int n = int.Parse(Console.ReadLine());
+string[] array = { "hello", "123", "world", "Denmark", "yes", "min" };
+string[] result = GetWord(array, n);
