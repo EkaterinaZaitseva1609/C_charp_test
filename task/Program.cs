7479303ece82df718704e0c8d4b9184cddcf0fab
ekaterina <zaitsevae1609@gmail.com>
2022-07-13 15:15:10 +0700

    do_task

--- task/Program.cs
+++ task/Program.cs
@@ -1,5 +1,17 @@
 ﻿// Задача 
 
+
+string Print(string[] array)
+{
+    int length = array.Length;
+    string output = String.Empty;
+    for (int i = 0; i < length; i++)
+    {
+        output += (array[i] + " ");
+    }
+    return output;
+}
+
 string[] GetWord(string[] array, int n)
 {
     string[] outputArray = new string[array.Length];
@@ -18,4 +30,8 @@ string[] GetWord(string[] array, int n)
 System.Console.WriteLine("Введит число N");
 int n = int.Parse(Console.ReadLine());
 string[] array = { "hello", "123", "world", "Denmark", "yes", "min" };
+
 string[] result = GetWord(array, n);
+string output = Print(result);
+System.Console.WriteLine(output);
+File.WriteAllText("output.txt", output);
